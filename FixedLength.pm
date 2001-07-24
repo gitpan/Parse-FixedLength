c04dd17ee91d4c498b2949144dae8e99b396abdc
Douglas Wilson <dougw@cpan.org>
2001-07-24 22:08:01 +0000

    Import of DOUGW/Parse-FixedLength-5.04 from CPAN.
    gitpan-cpan-distribution: Parse-FixedLength
    gitpan-cpan-version:      5.04
    gitpan-cpan-path:         DOUGW/Parse-FixedLength-5.04.tar.gz
    gitpan-cpan-author:       DOUGW
    gitpan-cpan-maturity:     released

--- FixedLength.pm
+++ FixedLength.pm
@@ -6,11 +6,11 @@ use strict;
 #	Public Global Variables
 #-----------------------------------------------------------------------
 use vars qw($VERSION);
-$VERSION   = '5.03';
+$VERSION   = '5.04';
 
 #=======================================================================
 sub new {
-    # Do the cargo cult object creation
+    # Do the cargo cult OO construction thing
     my $proto = shift;
     my $class = ref($proto) || $proto;
     my $self = bless {}, $class;
@@ -18,31 +18,52 @@ sub new {
     my $params = shift;
     my $delim = $params->{'delim'};
     my $spaces = $params->{'spaces'} ? 'a' : 'A';
-    my (@names, @lengths);
+    my (@names, @lengths, %justify);
     my $length = 0;
     if (defined $delim) {
         for (@$format) {
             my ($name, $len) = split $delim;
             push @names, $name;
+            ($len, my $chr) = _right_justify($len);
+            $justify{$name} = $chr if defined $chr;
             push @lengths, $len;
             $length += $len;
         }
     } else {
         for (my $i=0; $i < $#$format; $i+=2) {
-            push @names, $$format[$i];
-            push @lengths, $$format[$i+1];
-            $length += $$format[$i+1];
+            my $name = $$format[$i];
+            my $len = $$format[$i+1];
+            ($len, my $chr) = _right_justify($len);
+            $justify{$name} = $chr if defined $chr;
+            push @names, $name;
+            push @lengths, $len;
+            $length += $len;
         }
     }
     $self->{NAMES} = \@names;
     $self->{UNPACK} = join '', map { "$spaces$_" } @lengths;
     $self->{PACK} = join '', map { "A$_" } @lengths;
     $self->{LENGTH} = $length;
+    if (%justify and ! $params->{'no_justify'}) {
+     $self->{JUST} = 1;
+     $self->{JFIELDS} = \%justify;
+    }
     my %lengths;
     @lengths{@names} = @lengths;
     $self->{LENGTHS} = \%lengths;
     $self->{DEBUG} = 1 if $params->{'debug'};
     $self;
+}
+
+sub _right_justify {
+    my $len = shift;
+    my $chr;
+    if ((my $pos = index($len, "R")) >= 0) {
+        my $tmp_len = substr($len, 0, $pos);
+        $chr = (length($len) > $pos) ? substr($len,$pos+1,1) : ' ';
+        $len = $tmp_len;
+    }
+    return $len, $chr;
 }
 
 #=======================================================================
@@ -63,7 +84,15 @@ sub parse {
 #=======================================================================
 sub pack {
     my $parser = shift;
-    pack $parser->{PACK}, @{shift()}{@{$parser->{NAMES}}};
+    my $href = shift;
+    if ($parser->{JUST}) {
+        while (my ($name, $chr) =  each %{$parser->{JFIELDS}}) {
+            (my $field = $$href{$name}) =~ s/^\s+|\s+$//g;
+            my $len = $parser->length($name);
+            $$href{$name} = $chr x ($len-length($field)) . $field;
+        }
+    }
+    CORE::pack $parser->{PACK}, @$href{@{$parser->{NAMES}}};
 }
 #=======================================================================
 sub names {
@@ -76,13 +105,53 @@ sub length {
    $self->{LENGTHS}{$field};
 }
 #=======================================================================
+# Maybe not a clean OO way, this should be a different
+# object in a different class, but oh well...it ain't rocket science.
+sub converter {
+   my ($parser1, $parser2, $mappings, $defaults) = @_;
+   # Do the OO cargo cult dance
+   my $proto = shift;
+   my $class = ref($proto) || $proto;
+   my $self = bless {}, $class;
+   $self->{UNPACKER} = $parser1;
+   $self->{PACKER} = $parser2;
+   $self->{MAP} = { reverse %$mappings };
+   $self->{DEFAULTS} = $defaults;
+   $self;
+}
+#=======================================================================
+sub convert {
+    my $converter = shift;
+    my $data = shift;
+    my $unpacker = $converter->{UNPACKER};
+    my $packer   = $converter->{PACKER};
+    my $map_to   = $converter->{MAP};
+    my $defaults = $converter->{DEFAULTS};
+
+    my $data_in   = $unpacker->parse($data);
+    my $names_out = $packer->names;
+
+    # Map the data from input to output
+    my %data_out; @data_out{@$names_out} = map {
+        exists $map_to->{$_} ? $data_in->{$map_to->{$_}}
+      : exists $data_in->{$_} ? $data_in->{$_} : ''
+    } @$names_out;
+
+    # Default/Convert the fields
+    for (keys %$defaults) {
+        $data_out{$_} =
+        ref($defaults->{$_}) ?
+            $defaults->{$_}->($data_out{$_},$data_in) : $defaults->{$_};
+    }
+    $packer->pack(\%data_out);
+}
 
 1;
 __END__
 
 =head1 NAME
 
-Parse::FixedLength - parse a string containing fixed length fields into component parts
+Parse::FixedLength - parse a ascii string containing fixed length fields into component parts
 
 =head1 SYNOPSIS
 
@@ -95,7 +164,8 @@ Parse::FixedLength - parse a string containing fixed length fields into componen
     ]);
 
     my $data = 'Bob       Jones     1122 Main St.       ';
-    my $parsed = $parser->parse($data);
+    my $parsed_href = $parser->parse($data);
+    my $orig_data = $parser->pack($parsed_href);
 
     or:
 
@@ -126,12 +196,23 @@ This method takes an array reference of field names and
 lengths as either alternating elements, or delimited args in the
 same field.
 
+To right justify a field (during the 'pack' method), an "R" may
+be appended to the length of the field along with (optionally)
+the character to pad the string with. This is somewhat inefficient,
+so its only recommended if actually necessary to preserve the format
+during operations such as math or converting format lengths. If its
+not needed but you'd like to specify it anyway for documentation
+purposes, you can use the no_justify option below. Also, it does change
+the data in the hash ref argument.
+
 An optional hash ref may also be supplied which may contain the following:
 
  delim - The delimiter used to separate the name and length in
          the format array.
 
- spaces - If true, preserve trailing spaces in the parsed output.
+ spaces - If true, preserve trailing spaces during parse.
+
+ no_justify - Ignore the "R" format option during pack.
 
  debug  - Print field names and values during parsing (as a quick
           format validation check).
@@ -148,8 +229,8 @@ values if called in list context.
 =item pack
  $packed_str = $parser->pack($href_data_to_pack);
 
-This function takes a hash reference and returns a fixed length format
-output string.
+This function takes a hash reference of field names and values and
+returns a fixed length format output string.
 
 =item names()
 
@@ -164,6 +245,36 @@ Return an ordered arrayref of the field names.
 
 Returns the total length of all the fields, or of just one field name.
 
+=item converter()
+
+ $converter = $parser1->converter($parser2, \%mapping, \%defaults);
+
+Returns a format converting object. $parser1 is the parsing object
+to convert from, $parser2 is the parsing object to convert to.
+
+By default, common field names will be mapped from one format to the other.
+Fields with different names can be mapped from the first format to the
+other (or you can override the default) using the second argument
+(See examples).
+
+And defaults for any field in the target format can be supplied
+using the third argument, where the keys are the field names of
+the target format, and the value can be a scalar constant, or a
+subroutine reference where the first argument is simply the mapped
+value (or the empty string if there was no mapping), and the
+second argument is the entire hash reference that results from parsing
+the data with the 'from' parser object. E.g. if you were mapping
+from a separate 'zip' and 'plus_4' field to a 'zip_plus_4' field,
+you could supply in the default hash ref the following:
+
+ zip => sub { shift() . $$_[1]{plus_4} }
+
+=item convert()
+
+ $formatted_in_str = $converter->convert($formatted_out_str);
+
+Converts a string from one fixed length format to another.
+
 =back
 
 =cut
@@ -175,13 +286,11 @@ Returns the total length of all the fields, or of just one field name.
     my $parser = Parse::FixedLength->new([
         first_name => 10,
         last_name  => 10,
-        widgets_this_month => 5,
+        widgets_this_month => '5R0',
     ]);
 
     # Do a simple name casing of names
     # and print widgets projected for the year for each person
-    # (Numbers are right justified for cut n paste purposes,
-    # but will end up left justified to simplify this example)
     while (<DATA>) {
         warn "No record terminator found!\n" unless chomp;
         warn "Short Record!\n" unless $parser->length == length;
@@ -195,6 +304,41 @@ Returns the total length of all the fields, or of just one field name.
     BOB       JONES        24
     JOHN      SMITH         5
     JANE      DOE           7
+
+    A alternate way if we're converting formats:
+
+    my $parser1 = Parse::FixedLength->new([
+        first_name => 10,
+        last_name  => 10,
+        widgets_this_month => '5R0',
+    ]);
+
+    my $parser2 = Parse::FixedLength->new([
+        seq_id     => 10,
+        first_name => 10,
+        last_name  => 10,
+        country    =>  3,
+        widgets_this_year => '10R0',
+    ]);
+
+    my $converter = $parser1->converter($parser2, {
+        widgets_this_month => widgets_this_year,
+    },{
+        seq_id => do { my $cnt = '0' x $parser2->length('seq_id');
+                       sub { ++$cnt };
+                     },
+        widgets_this_year => sub { 12 * shift },
+        country => 'USA',
+    });
+    
+
+    # Do a simple name casing of names
+    # and print widgets projected for the year for each person
+    while (<DATA>) {
+        warn "No record terminator found!\n" unless chomp;
+        warn "Short Record!\n" unless $parser1->length == length;
+        print $converter->convert($_), "\n";
+    }
 
 =head1 AUTHOR
 
--- test.pl
+++ test.pl
@@ -6,7 +6,7 @@
 # Change 1..1 below to 1..last_test_to_print .
 # (It may become useful if the test is moved to ./t subdirectory.)
 
-BEGIN { $| = 1; print "1..7\n"; }
+BEGIN { $| = 1; print "1..8\n"; }
 END {print "not ok 1\n" unless $loaded;}
 use Parse::FixedLength;
 $loaded = 1;
@@ -23,36 +23,54 @@ my $parser = Parse::FixedLength->new([qw(
  last_name:10
  address:20
 )], {delim=>":"});
-if (defined $parser) {
-    print "ok 2\n";
-} else {
-    print "not ok 2\n";
-}
+
+my $not = 'not ';
+print $not unless defined $parser;
+print "ok 2\n";
 
 my $data = 'Bob       Jones     1122 Main St.       ';
 my $href =  $parser->parse($data);
-if ($href->{first_name} eq 'Bob') {
-    print "ok 3\n";
-} else {
-    print "not ok 3\n";
-}
-if ($href->{last_name} eq 'Jones') {
-    print "ok 4\n";
-} else {
-    print "not ok 4\n";
-}
-if ($href->{address} eq '1122 Main St.') {
-    print "ok 5\n";
-} else {
-    print "not ok 5\n";
-}
-if ($parser->length == 40) {
-    print "ok 6\n";
-} else {
-    print "not ok 6\n";
-}
-if ($parser->length('first_name') == 10) {
-    print "ok 7\n";
-} else {
-    print "not ok 7\n";
-}
+
+print $not unless $href->{first_name} eq 'Bob';
+print "ok 3\n";
+
+print $not unless $href->{last_name} eq 'Jones';
+print "ok 4\n";
+
+print $not unless $href->{address} eq '1122 Main St.';
+print "ok 5\n";
+
+print $not unless $parser->length == 40;
+print "ok 6\n";
+
+print $not unless $parser->length('first_name') == 10;
+print "ok 7\n";
+
+my $parser1 = Parse::FixedLength->new([
+    first_name => 10,
+    last_name  => 10,
+    widgets_this_month => '5R0',
+]);
+
+my $parser2 = Parse::FixedLength->new([
+    seq_id     => 10,
+    first_name => 10,
+    last_name  => 10,
+    country    =>  3,
+    widgets_this_year => '10R0',
+]);
+
+my $converter = $parser1->converter($parser2, {
+    widgets_this_month => widgets_this_year,
+},{
+    seq_id => do { my $cnt = '0' x $parser2->length('seq_id');
+                   sub { ++$cnt };
+                 },
+    widgets_this_year => sub { 12 * shift },
+    country => 'USA',
+});
+
+my $str_in = 'BOB       JONES        24';
+my $str_out = $converter->convert($str);
+print $not unless $str_out = '0000000001BOB       JONES     USA0000000288';
+print "ok 8\n";
